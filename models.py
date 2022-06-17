6756acc9e0206c050e6d191cf330a5acbe3e3870
NGUYEN VAN TUAN <tuandz@gmail.com>
2022-06-17 13:44:45 +0700

    2

--- models.py
+++ models.py
@@ -1,6 +1,10 @@
-print("Thay đổi để nghịch code")
-print("Thay đổi để nghịch code")
-print("Thay đổi để nghịch code")
-print("Thay đổi để nghịch code")
-print("lalalalalalallalalalalala")
-print("hahahahahahahahhahahahahha")
+from re import A, M
+from socket import AI_ADDRCONFIG
+
+
+AI_ADDRCONFIGnadsmd
+  ArgumentDefaultsHelpFormatter
+  A
+  asnkda;
+  aM
+  
